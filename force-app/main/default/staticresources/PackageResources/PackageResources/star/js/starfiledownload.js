2c0e9d16242e9ba039ad30d27172b83fc1ac29f8
vmundlye <vmundlye@verasolutions.org>
2019-07-19 15:39:55 +0530

    19-July, 4th commit [ci skip]

--- force-app/main/default/staticresources/PackageResources/PackageResources/star/js/starfiledownload.js
+++
@@ -1,7 +0,0 @@
-// This method checks the file format and calls a
-// method accordingly to download a file in that file format
-function downloadFile(object, fileFormat) {
-    if (EXCEL_FILE_FORMAT == fileFormat) {
-        initiateExcelDownload(object);
-    }
-}
